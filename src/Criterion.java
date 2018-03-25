831e0f7567c9a383d014122d23fb3be0960d24ed
Tomasz Pęcak <pectom@o2.pl>
2018-03-25 11:20:35 +0200

    Initial commit

---
+++ src/Criterion.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Criterion {
+    private String name;
+    private Matrix matrix;
+    private Criterion parent;
+    private LinkedList<Criterion> children = new LinkedList<>();
+
+
+}
